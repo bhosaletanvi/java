890803cee6dfb461287a4eb7125f3b411c0f948f
Tanvi <bhosaletanvi59@gmail.com>
2026-04-04 20:18:20 +0530

    file added

---
+++ p8.java
@@ -0,0 +1,10 @@
+class demo{
+    public static void main(String[] args) {
+        System.out.println("In demo class main ");
+    }
+}
+class codeX{
+    public static void main(String[] args) {
+        System.out.println("In codeX class main");
+    }
+}
---
+++ p9.java
@@ -0,0 +1,10 @@
+class demo{
+    public static void main(String[] args) {
+        System.out.println("In demo class main ");
+    }
+}
+class codeX{
+     static void fun() {
+        System.out.println("In codeX class fun");
+    }
+}
