531f95b156701d0081687da9417fc08b2f4dad99
Tanvi <bhosaletanvi59@gmail.com>
2026-04-12 01:28:09 +0530

    p25,p26,p27,p28 file added

---
+++ p29.java
@@ -0,0 +1,12 @@
+class demo{
+	public static void main(String [] args ){
+		int a= 10;
+		System.out.println(a);
+		demo.fun();
+	}
+	static void fun(){
+		a=20;
+		System.out.println(a);
+		
+}
+}
---
+++ p30.java
@@ -0,0 +1,12 @@
+class demo{
+	public static void main(String [] args ){
+		int a= 10;
+		System.out.println(a);
+		demo.fun();
+	}
+	static void fun(){
+		int a=20;
+		System.out.println(a);
+		
+}
+}
