be070d889fb0100babc36c2ec5c7f1bdcad5504a
Tanvi <bhosaletanvi59@gmail.com>
2026-04-12 01:31:47 +0530

    p29,30,31,32,33 file added

---
+++ p33.java
@@ -0,0 +1,18 @@
+class demo{
+	public static void main(String [] args ){
+		int a= 10,b=20;
+		System.out.println(a);
+		System.out.println(10);
+		System.out.println("a");
+		System.out.println('a');
+
+		String s="abc";
+		System.out.println(s);
+
+		
+
+
+
+
+	}
+}
