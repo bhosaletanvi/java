a2474176ecfc4bb1a83f5c42027fc3e51f386582
Tanvi <bhosaletanvi59@gmail.com>
2026-04-18 20:35:13 +0530

    file added

---
+++ variable_datatype/p52.java
@@ -0,0 +1,6 @@
+class demo{
+	public static void main(String [] args){
+		long a=1037082489;
+		System.out.println(a);	
+	}
+}
---
+++ variable_datatype/p53.java
@@ -0,0 +1,6 @@
+class demo{
+	public static void main(String [] args){
+		String str ="tanvi bhosale";
+		System.out.println(str);	
+	}
+}
