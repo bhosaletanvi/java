0a82c0cbef6194ed549165d28366fd67aab51972
Tanvi <bhosaletanvi59@gmail.com>
2026-04-20 21:38:15 +0530

    files added

---
+++ operators/p75.java
@@ -0,0 +1,12 @@
+class demo{
+	public static void main(String [] args){
+		int a=5;
+		int b= 7;
+		boolean c = ++a < ++b && ++a > ++b; 
+		System.out.println(a);	
+		System.out.println(b);
+		System.out.println(c);		
+		
+	
+	}
+}
