7e7550e9c4a40e511bb5c529470c8d59f708aafe
Tanvi <bhosaletanvi59@gmail.com>
2026-05-01 15:12:06 +0530

    file added

---
+++ control_statements/if_else/p87.java
@@ -0,0 +1,13 @@
+class demo{
+	public static void main(String [] args){
+		int age=5;
+		if(age>=18){	
+			System.out.println("valid");	
+		}
+		else {
+			System.out.println("invalid");	
+		}
+
+
+	}
+}
---
+++ control_statements/if_else/p88.java
@@ -0,0 +1,14 @@
+class demo{
+	public static void main(String [] args){
+		int codes=2;
+		boolean apti=true;
+		if(apti==true && codes==2){	
+			System.out.println("prime package");	
+		}
+		else {
+			System.out.println("acme");	
+		}
+
+
+	}
+}
