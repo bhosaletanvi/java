18c9c05d7e6dfa54e671dd2e856e96f8d6f2a53c
Tanvi <bhosaletanvi59@gmail.com>
2026-05-01 15:14:38 +0530

    file added

---
+++ control_statements/if_else/p89.java
@@ -0,0 +1,14 @@
+class demo{
+	public static void main(String [] args){
+		int codes=2;
+		boolean apti=false;
+		if(apti && codes==2){	
+			System.out.println("prime package");	
+		}
+		else {
+			System.out.println("acme");	
+		}
+
+
+	}
+}
