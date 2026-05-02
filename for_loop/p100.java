8c29a28032c25bf3478b8b5c2bdc9ffda02d440d
Tanvi <bhosaletanvi59@gmail.com>
2026-05-02 21:31:24 +0530

    file added

---
+++ for_loop/p100.java
@@ -0,0 +1,8 @@
+class demo{
+	public static void main(String [] args){
+	
+		String s ="5";
+		System.out.print(s+5+s+10+5+s);
+	
+	}
+}
---
+++ for_loop/p101.java
@@ -0,0 +1,9 @@
+class demo{
+	public static void main(String [] args){
+	
+		int a=10;
+		int b =20;
+		System.out.print("Addition of two numbers : "+(a+b));
+	
+	}
+}
